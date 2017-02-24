10a5dd539ee1c357a9d7aefa17fc7a4441eddef8
Félix Pelletier <felixpelletier23@gmail.com>
2017-02-24 11:28:49 -0500

    commMemoirePartagee avancé

--- tp3/build.sh
+++ tp3/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -lrt decodeur.c commMemoirePartagee.c allocateurMemoire.c jpgd.cpp utils.c -o decodeur
-g++ -lrt compositeur.c commMemoirePartagee.c allocateurMemoire.c utils.c -o compositeur
+g++ -lrt -lpthread decodeur.c commMemoirePartagee.c allocateurMemoire.c jpgd.cpp utils.c -o decodeur
+g++ -lrt -lpthread compositeur.c commMemoirePartagee.c allocateurMemoire.c utils.c -o compositeur
