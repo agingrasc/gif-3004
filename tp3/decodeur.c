91f3482b64fda8c073c8f856741bfc37ba553562
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-03-03 12:24:22 -0500

    structure decoder OK
    * wip acquerir info video
    * copie dans memoire partage
    * init memoire partagee ecrivain

--- tp3/decodeur.c
+++ tp3/decodeur.c
@@ -1,21 +1,18 @@
 #include "allocateurMemoire.h"
 #include "commMemoirePartagee.h"
-#include "utils.h"
-
-#include "jpgd.h"
 
 // Gestion des ressources et permissions
-#include <sys/resource.h>
 
 // Définition de diverses structures pouvant vous être utiles pour la lecture d'un fichier ULV
 #define HEADER_SIZE 4
 const char header[] = "SETR";
+#define INFO_SIZE 20
 
-struct videoInfos{
-        uint32_t largeur;
-        uint32_t hauteur;
-        uint32_t canaux;
-        uint32_t fps;
+struct videoInfos {
+    uint32_t largeur;
+    uint32_t hauteur;
+    uint32_t canaux;
+    uint32_t fps;
 };
 
 /******************************************************************************
@@ -40,24 +37,104 @@ struct videoInfos{
  * @param ptr
  * @return
  */
-int validate_header(void *ptr) {
-    char* vid_header = (char*) ptr;
+int validate_header(FILE *file) {
     for (int i = 0; i < HEADER_SIZE; i++) {
-        if (vid_header[i] != header[i]) {
-            return 0;
+        if (getc(file) != header[i]) {
+            return 1;
         }
     }
 
-    return 1;
+    return 0;
 }
 
+uint32_t read_uint32(FILE *file) {
+    uint32_t arg = 0;
+    char data[4];
+    int idx = 0;
+    while (data[idx] = getc(file) != NULL && idx < 4) {
+        idx++;
+    }
 
-int main(int argc, char* argv[]){
-    
+    if (idx < 3) {
+        printf("Erreur lecture info video");
+        exit(1);
+    }
+
+    memcpy(&arg, data, 4);
+    printf("arg read: %d\n", arg);
+    return arg;
+}
+
+int main(int argc, char *argv[]) {
+
+    int err, current_idx;
     // Écrivez le code de décodage et d'envoi sur la zone mémoire partagée ici!
     // N'oubliez pas que vous pouvez utiliser jpgd::decompress_jpeg_image_from_memory()
     // pour décoder une image JPEG contenue dans un buffer!
     // N'oubliez pas également que ce décodeur doit lire les fichiers ULV EN BOUCLE
 
-    //
+    //recuperer les arguments: file, flux_sortie
+    char *filename;
+    char *output_flux;
+    filename = (argv[1]);
+    output_flux = (argv[2]);
+    printf("Args: %s, %s\n", filename, output_flux);
+
+    //ouverture fichier video
+    FILE *video_file = fopen(filename, "r");
+    if (video_file == NULL) {
+        printf("Echec de l'ouverture du fichier video: %s", filename);
+        return 1;
+    }
+    fseek(video_file, 0L, SEEK_END);
+    size_t mem_size = ftell(video_file) - 20;
+    rewind(video_file);
+
+    //init memoire ecrivain
+    memPartage mem;
+    memPartageHeader memHeader;
+    err = initMemoirePartageeEcrivain(output_flux, &mem, mem_size, &memHeader);
+    if (err) {
+        printf("Echec init memoire partage ecrivain (decodeur) pour %s.", filename);
+        return 1;
+    }
+
+    //valider header
+    err = validate_header(video_file);
+    if (err) {
+        printf("Header invalide! %s", filename);
+        return 1;
+    }
+
+    //extraire meta information (larger, hauteur, req_comp, fps)
+    videoInfos video_info;
+    video_info.largeur = read_uint32(video_file);
+    video_info.hauteur = read_uint32(video_file);
+    video_info.canaux = read_uint32(video_file);
+    video_info.fps = read_uint32(video_file);
+    printf("Info lu:\nlargeur: %d\nhauteur: %d\ncanaux: %d\nfps: %d\n", video_info.largeur, video_info.hauteur,
+           video_info.canaux, video_info.fps);
+
+    current_idx = 4; //nbr de char lu dans la video
+
+    //mettre le fichier en memoire
+    char data;
+    current_idx = 0;
+    fseek(video_file, 0L, 0);
+    while (data = getc(video_file) != EOF) {
+        mem.data[current_idx] = data;
+        current_idx++;
+    }
+    fclose(video_file);
+
+    //boucle continu
+    while (1) {
+        //extraire taille prochaine image
+        int image_size = (int) -32;
+
+        //decoder
+        //jpgd::decompress_jpeg_image_from_memory(video, image_size, &width, &height, &actual_comp, req_comp);
+        break;
+    }
+    return 0;
 }
