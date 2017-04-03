20ccf9d997b7ea0336d60e0810558313a8eb5e61
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-04-03 16:21:47 -0400

    base cmakes

--- arm-linux-gnueabihf.cmake
+++ arm-linux-gnueabihf.cmake
@@ -4,21 +4,17 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
 set(TOOLCHAIN_PREFIX "/opt/cross/raspberry-pi/arm-linux-gnueabihf")
+
 # binaire pour la compilation
-
-#link_directories("${TOOLCHAIN_PREFIX}/arm-linux-gnueabihf/lib" "/opt/cross/raspberry-pi/rootfs/lib")
-#set(CMAKE_LINKER "${TOOLCHAIN_PREFIX}/arm-linux-gnueabihf/bin/ld")
-#set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
-#set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_LINKER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET>")
-#set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_LINKER} <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
-#set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_LINKER} <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET>")
-
 set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}/bin/arm-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}/bin/arm-linux-gnueabihf-g++")
 
 # sysroot de compilation croisee
 set(CMAKE_SYSROOT "/opt/cross/raspberry-pi/rootfs")
 set(CMAKE_FIND_ROOT_PATH "/opt/cross/raspberry-pi/rootfs")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
 
 # parametre pour la recherche
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
